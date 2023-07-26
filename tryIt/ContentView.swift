d0fb6f05b2755d4d74252ff8dbc36e411578c44c
riya <rzingade506@student.fuhsd.org>
2023-07-26 13:02:54 -0700

    Initial Commit

---
+++ tryIt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tryIt
+//
+//  Created by Riya Zingade on 7/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ tryIt/tryItApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryItApp.swift
+//  tryIt
+//
+//  Created by Riya Zingade on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct tryItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
